2386c4334df1ee271f6adb5f6be2a802f1bb3a34
shwethabbaliga <shwethab.baliga@gmail.com>
2026-07-15 12:13:09 +0530

    push

---
+++ lib/firmware_lib.py
@@ -0,0 +1,11 @@
+import lib_main
+
+class firmware_validation:
+
+    def __init__(self):
+        print("add kernel, firmware versions here")
+        
+    def firmware_version(self):
+        pass
+
+    def 
