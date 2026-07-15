5ce67753b443206bed5e7fafaf3ecab2c46df886
shwethabbaliga <shwethab.baliga@gmail.com>
2026-07-15 12:10:26 +0530

    First commit

---
+++ lib/common_commands.py
@@ -0,0 +1,30 @@
+import lib_main
+
+class common_commands(lib_main.main_class):
+    def uname(self,option):
+        command = "uname " + option
+        self.run_command("uname " + option)
+        pass
+    
+    def changelog(self,package_name):
+        print("get ur changelog of the package here")
+
+    def dmesg_log(self):
+        pass
+
+    def journalctl_log(self,**kwargs):
+        pass
+
+    def hostname(self):
+        print('provide hostname')
+
+    def architecture(self):
+        print("architecture")
+
+    def release(self):
+        print("cat /etc/oracle-release")
+
+
+command = common_commands()
+
+command.uname("-r")
