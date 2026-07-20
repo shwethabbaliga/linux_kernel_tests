40719a946cd3d0abab1cc239bd1d2b8ff992a76b
shwethabbaliga <shwethab.baliga@gmail.com>
2026-07-20 11:19:03 +0530

    changes

--- lib/lib_main.py
+++ lib/lib_main.py
@@ -5,6 +5,6 @@ class main_class:
     def run_command(self,command):
         status = subprocess.run(command,shell=True,capture_output=True,text=True)
         print (command)
-        if status.returncode: 
+        if not status.returncode: 
             print (f"command execution is perfect o/p is following {status.stdout}")
 
