96d7488bae187c9044a371b7419433f7c6a5ff47
shwethabbaliga <shwethab.baliga@gmail.com>
2026-07-20 10:44:07 +0530

    changes

--- lib/lib_main.py
+++ lib/lib_main.py
@@ -3,8 +3,7 @@ from abc import ABC , abstractmethod
 
 class main_class:
     def run_command(self,command):
-        status = 0 
-        #status = subprocess.run(command,shell=True,capture_output=True,Text=True)
+        status = subprocess.run(command,shell=True,capture_output=True,Text=True)
         print (command)
         if status.returncode: 
             pass
